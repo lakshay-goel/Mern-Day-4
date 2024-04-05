f4c392c7562efc83d54590fe556ea511e4e3f425
Lakshay Goel <102369062+lakshay-goel@users.noreply.github.com>
2024-04-05 20:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// let a = new String("hello");
+// let b = "hello";
+// if(a==b){
+//     console.log("yes");
+
+// }
+// else
+// {
+//     console.log("no")
+// }
+
+
+// const obj ={
+//     "name": "Ajay",
+//     "lastname": "singh"
+// }
+// const str=JSON.stringify(obj);
+// document.write(strj);
+
+
+
+
+// const obj ={
+//     "name": "Ajay",
+//     "lastname": "singh"
+// }
+//     obj.age=20;
+//     console.log(obj);
+
+
+// let arr=[1,5,34];
+// // arr=[2,3,4];
+// arr[10]=20;
+// console.log(arr);
+
+// const obj ={
+//     "name": "Ajay",
+//     "lastname": "singh"
+// }
+// console.log(obj);
+// const arr= [1,5,34];
+// console.log(arr);
+// console.log(typeof(arr));
+// conSSSsole.log(typeof(obj));
+
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(obj));
+
+// checkifobject([1,2,3,4])
+
+
+// const arr=['name', 2];
+// const obj={
+//     fullname:'ajay',
+//     age:21,
+// };
+// for (let i of arr){
+//     console.log(i);
+// }
+
+
+// console.log(window);
+// console.log(innerHeight);
+// console.log(innerWidth);
+
+
+// console.log(window.document)
+// // console.log(window)
+// console.dir(window)
+
+
+
+// const res
+// =
+// // document.getElementById('pt1');
+// document.querySelectorAll('h3');
+// // document.getElementsByTagName('p');
+// console.log(res);
+
+
+
+// const ne=document.createElement("h3");
+// ne.innerText="Dynamic text";
+// console.log(ne);
+// document.body.appendChild(ne);
+
+
+
+// const ne = document.createElement("h3");
+
+// ne.innerText="Dynamic text";
+// console.log(ne);
+// const firstdiv = document.getElementsByTagName("h3");
+// firstdiv[0].appendChild(ne);
+//         // ***************************div copy vs shalow copy **********************************imp************************** */   
+
+
+
+// const firstnamechange=() =>{
+//         console.log('firstname');
+// }
+
+
+
+// const submitform=(e)=>{
+//         e.preventdefault();
+//         for(let i=1;i<4;i++){
+//                 console.log(e.target[i].value);
+//         }
+//         console.log(e);        
+// }
---
+++ script1.js
@@ -0,0 +1,29 @@
+// const div = 
+// document.querySelector('div');
+// console.log(div);
+// // div[0].innerHTML = "<p>hello</P>";
+// div.removeChild(c);
+
+
+// const res=document.querySelector('div');
+// const c=document.querySelector('p');
+// res.removeChild(c);
+
+
+
+// function getinfo(){
+//     const d=document.querySelector("div");//on click on button get name
+//     d.append("my name is lakshay");
+//     // console.log("Btn Clicked");
+// }\\
+
+
+
+
+function getinfo(e)
+{
+    console.log(e);
+    e.target.style.backgroundColor='yellow';
+    e.target.style.color='red';
+    
+}
